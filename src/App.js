a9cf134a9817e74f436a0049a664346990d54aff
Jonathan Bertoldi <contato@jonathanbertoldi.com.br>
2019-11-26 00:53:22 -0300

    Add a search field to the monter cards

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -19,9 +20,14 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField  } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())  
+    );
     return(
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <input onChange={e => this.setState({ searchField: e.target.value })} type="search" placeholder="Search monsters" />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     )
   }
